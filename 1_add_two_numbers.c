bbf5c71672cc479f44af3cf590abefc214c65f44
Krishi23 <124159809+Krishi23@users.noreply.github.com>
2023-02-01 18:35:50 +0530

    \n changed to spaces

--- 1_add_two_numbers.c
+++ 1_add_two_numbers.c
@@ -3,10 +3,10 @@ void main()
 {
     float a,b,sum;
     printf("*********Addition of two numbers***********\n");
-    printf("Enter the first number for addition:\n");
+    printf("Enter the first number for addition : ");
     scanf("%f",&a);
-    printf("Enter the Second number for addition:\n");
+    printf("Enter the Second number for addition : ");
     scanf("%f",&b);
     sum = a+b;
-    printf("The Sum of the two numbers is: %f",sum);
+    printf("The Sum of the two numbers is : %f",sum);
 }
