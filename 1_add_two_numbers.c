c36fe2e011222330bf2654327de281f0e65f8904
Krishi23 <124159809+Krishi23@users.noreply.github.com>
2023-02-01 18:31:02 +0530

    Made code more readable when taking input

--- 1_add_two_numbers.c
+++ 1_add_two_numbers.c
@@ -3,9 +3,9 @@ void main()
 {
     float a,b,sum;
     printf("*********Addition of two numbers***********\n");
-    printf("Enter the first number:");
+    printf("Enter the first number for addition:");
     scanf("%f",&a);
-    printf("Enter the Second number:");
+    printf("Enter the Second number for addition:");
     scanf("%f",&b);
     sum = a+b;
     printf("The Sum of the two numbers is: %f",sum);
