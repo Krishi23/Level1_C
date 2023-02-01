863089f96953d583768959b97e9788f650b23ff3
Krishi23 <124159809+Krishi23@users.noreply.github.com>
2023-02-01 18:18:52 +0530

    First Commit

---
+++ 1_add_two_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+float a,b,sum;
+printf("This Program Executes addition of two numbers\n");
+printf("Enter the first number:");
+scanf("%f",&a);
+printf("Enter the Second number:");
+scanf("%f",&b);
+sum = a+b;
+printf("The Sum of the two numbers is: %f",sum);
+}
